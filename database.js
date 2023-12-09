cb39b958712de37840f22b8cfdbf72e1e9fd1e71
Krypt0n36 <belani.hassen@gmail.com>
2023-12-09 15:39:30 +0100

    Autoconf on db cred rotation

--- database.js
+++ database.js
@@ -5,13 +5,8 @@ dotenv.config();
 // ssl: { rejectUnauthorized: false }
 
 const pool = new Pool({
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_PASW,
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
-    
 })
 
 
